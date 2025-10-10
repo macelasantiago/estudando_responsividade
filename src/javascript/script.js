522016b6ec286744ef5f4f8bb7bd6aaf0743135f
Marcela Santiago <macelassantiago@gmail.com>
2025-10-10 10:25:11 -0300

    Primeiro commit do estudo

---
+++ src/javascript/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('#mobile_btn').on('click', function() {
+        $('#mobile_menu').toggleClass('active');
+        $(this).find('i').toggleClass('fa-bars fa-xmark');
+    });
+});
